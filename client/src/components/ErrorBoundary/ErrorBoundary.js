135ed48e799dea9f1fa0c8323d5c2d483b084af6
Gennady Barsegyan <54933269+gennady-bars@users.noreply.github.com>
2020-06-15 14:04:30 +0300

    Update ErrorBoundary.js

--- client/src/components/ErrorBoundary/ErrorBoundary.js
+++ client/src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class extends Component {
+export default class ErrorBoundary extends Component {
   state = {
     hasError: false,
   };
