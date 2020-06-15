9d30189c3d788cb38766fe0c0f776291d76261f2
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-15 13:21:44 +0300

    add ErrorBoundary

--- client/src/index.js
+++ client/src/index.js
@@ -2,31 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import js_cookie from 'js-cookie'
-import jwt_decode from 'jwt-decode'
+import js_cookie from "js-cookie";
+import jwt_decode from "jwt-decode";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App";
 import store from "./store/store";
 import { setUser, logoutUser } from "./store/actions/authActions";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
-const jwt = js_cookie.get('jwt')
+const jwt = js_cookie.get("jwt");
 
 if (jwt) {
-  const decoded = jwt_decode(jwt)
-  store.dispatch(setUser(decoded))
+  const decoded = jwt_decode(jwt);
+  store.dispatch(setUser(decoded));
 
-  const currentTime = Date.now() / 1000
+  const currentTime = Date.now() / 1000;
 
   if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser())
-    window.location.href = '/login'
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
   }
 }
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
